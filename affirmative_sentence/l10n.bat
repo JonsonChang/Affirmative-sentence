fbe8678170cec96731845364c13747e148aaf865
張小捲 <puperchang@gmail.com>
2025-05-13 13:28:17 +0800

    00

--- affirmative_sentence/l10n.bat
+++ affirmative_sentence/l10n.bat
@@ -11,6 +11,22 @@ set OUTPUT_DIR=%PROJECT_ROOT%\lib\l10n
 REM 設定原始目錄
 set SOURCE_DIR=%PROJECT_ROOT%\.dart_tool\flutter_gen\gen_l10n
 
+echo 正在執行 flutter gen-l10n...
+call flutter gen-l10n
+
+echo flutter gen-l10n 執行成功。
+
+REM 檢查目標目錄是否存在，如果不存在則建立
+if not exist "%OUTPUT_DIR%" (
+    echo 目標目錄 "%OUTPUT_DIR%" 不存在，正在建立...
+    mkdir "%OUTPUT_DIR%"
+    if %errorlevel% neq 0 (
+        echo 無法建立目錄 "%OUTPUT_DIR%"。
+        exit /b %errorlevel%
+    )
+    echo 目標目錄建立成功。
+)
+
 REM 複製檔案
 echo 正在將檔案從 "%SOURCE_DIR%" 複製到 "%OUTPUT_DIR%"...
 xcopy "%SOURCE_DIR%\*" "%OUTPUT_DIR%\" /s /y
